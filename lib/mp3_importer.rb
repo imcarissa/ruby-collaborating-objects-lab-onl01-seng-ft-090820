c98706dd30abe32da52200aeb28f3ce122d19126
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-25 19:26:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,6 +1,10 @@
 class MP3Importer
   
+<<<<<<< HEAD
   attr_reader :path
+=======
+  attr_accessor :path
+>>>>>>> 2bb232365360f94dc68d9a1f9333e09af4abb9cc
   
   def initialize(filepath)
     @path = filepath
@@ -12,7 +16,12 @@ class MP3Importer
   end
   
   def import
+<<<<<<< HEAD
     files.each {|song| Song.new_by_filename(song)}
   end
   
+=======
+    
+  end
+>>>>>>> 2bb232365360f94dc68d9a1f9333e09af4abb9cc
 end
--- lib/song.rb
+++ lib/song.rb
@@ -2,14 +2,21 @@ class Song
   
   attr_accessor :name, :artist
   
+<<<<<<< HEAD
   @@all = []
   
   def initialize(name)
     @name = name
+=======
+  def initialize(name)
+    @name = name
+    @songs = []
+>>>>>>> 2bb232365360f94dc68d9a1f9333e09af4abb9cc
     @@all << self
   end
   
   def self.all 
+<<<<<<< HEAD
     @@all 
   end
   
@@ -24,6 +31,16 @@ class Song
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
+=======
+    @all
+  end
+  
+  def self.new_by_filename(filename)
+    split_name = filename.split(" - ")
+    song = self.new(split_name[1])
+    artist = Artist.find_or_create_by_name(split_name[0])
+    song.artist = artist
+>>>>>>> 2bb232365360f94dc68d9a1f9333e09af4abb9cc
   end
   
 end
